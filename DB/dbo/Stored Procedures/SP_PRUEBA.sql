984d0d1fd446002ba6609676ba469ed936d1f489
emarzuca <153043343+emarzuca@users.noreply.github.com>
2024-04-03 16:08:23 -0600

    prueba

---
+++ DB/dbo/Stored Procedures/SP_PRUEBA.sql
@@ -0,0 +1,11 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SP_PRUEBA
+
+AS
+BEGIN
+	SELECT * FROM AREAS
+END
